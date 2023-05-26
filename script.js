05a6ad95c4ddf91852c18e38eb3fc5cf71266ecc
teddy <teddy@teddy.email>
2023-05-26 23:01:33 +0200

    basic implementation of hashing

---
+++ script.js
@@ -0,0 +1,14 @@
+let generate_button =  document.getElementById("generate_hash");
+let url_input = document.getElementById("url_input");
+let hash_result_text = document.getElementById("hash_result");
+
+function sha512(str) {
+  return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
+    return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+  });
+}
+
+generate_button.onclick = async () => {
+  hash_result_text.innerText = (await sha512(url_input.textContent)).toString();
+}
+
