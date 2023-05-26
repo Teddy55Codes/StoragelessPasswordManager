6bfef119f69d886c806e378d38da9421e61754f4
teddy <teddy@teddy.email>
2023-05-26 23:09:01 +0200

    your password lable now only visible if already generated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let generate_button =  document.getElementById("generate_hash");
 let url_input = document.getElementById("url_input");
 let hash_result_text = document.getElementById("hash_result");
+let hash_result_title = document.getElementById("hash_result_title");
 
 function sha512(str) {
   return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
@@ -10,5 +11,6 @@ function sha512(str) {
 
 generate_button.onclick = async () => {
   hash_result_text.innerText = (await sha512(url_input.textContent)).toString();
+  hash_result_title.style.visibility='visible';
 }
 
