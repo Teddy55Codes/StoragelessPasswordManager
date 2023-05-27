0c3755ae7ada9f81208ca69435e77bce52210454
Teddy55Codes <teddy@teddy.email>
2023-05-27 04:26:07 +0200

    add hash algorithem selection

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ let generate_button =  document.getElementById("generate_hash");
 let url_input = document.getElementById("url_input");
 let hash_result_text = document.getElementById("hash_result");
 let hash_result_title = document.getElementById("hash_result_title");
+let hash_algorithm_selection = document.getElementById("hash_algorithm_selection");
 
-function sha512(str) {
-  return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
+function hasher(str, hash_function) {
+  return crypto.subtle.digest(hash_function, new TextEncoder("utf-8").encode(str)).then(buf => {
     return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
   });
 }
 
 generate_button.onclick = async () => {
-  hash_result_text.innerText = (await sha512(url_input.value)).toString();
+  hash_result_text.innerText = (await hasher(url_input.value, hash_algorithm_selection.options[hash_algorithm_selection.selectedIndex].value)).toString();
   hash_result_title.style.visibility='visible';
 }
 
