e4a1af6969ab4fd1d89727eb7cb46fe69da3d908
Teddy55Codes <teddy@teddy.email>
2023-05-29 02:29:04 +0200

    added salt input #1

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const generate_hash = document.getElementById("generate_hash");
-const url_input = document.getElementById("url_input");
 const hash_algorithm_selection = document.getElementById("hash_algorithm_selection");
 const credential_type_username_url = document.getElementById("credential_type_username_url");
 const credential_type_url = document.getElementById("credential_type_url");
 const credentials_form_username = document.getElementById("credentials_form_username");
 const credentials_form_url = document.getElementById("credentials_form_url");
+const credentials_form_salt = document.getElementById("credentials_form_salt");
 
+const salt_start_text = ":START_SALT:";
 const display_for_initially_hidden_elements = "inline";
 
 // un-check on F5 refresh (otherwise a radio button can be checked while not credential input fields visible)
@@ -21,24 +22,28 @@ function hasher(str, hash_function) {
 credential_type_username_url.onclick = () => {
     credentials_form_username.style.display = display_for_initially_hidden_elements;
     credentials_form_url.style.display = display_for_initially_hidden_elements;
+    credentials_form_salt.style.display = display_for_initially_hidden_elements
     generate_hash.style.display = display_for_initially_hidden_elements;
 }
 
 credential_type_url.onclick = () => {
     credentials_form_username.style.display = "none";
+    credentials_form_salt.style.display = display_for_initially_hidden_elements
     credentials_form_url.style.display = display_for_initially_hidden_elements;
     generate_hash.style.display = display_for_initially_hidden_elements;
 }
 
 generate_hash.onclick = async () => {
-    let hash_result = document.getElementById("hash_result")
+    let hash_result = document.getElementById("hash_result");
+    let url_input = document.getElementById("url_input");
+    let salt_input = document.getElementById("salt_input");
 
     if (credential_type_username_url.checked) {
         let username_input = document.getElementById("username_input")
-        hash_result.innerHTML = (await hasher(username_input.value + "@" + url_input.value, hash_algorithm_selection.options[hash_algorithm_selection.selectedIndex].value)).toString();
+        hash_result.innerHTML = (await hasher(username_input.value + "@" + url_input.value + salt_start_text + salt_input.value, hash_algorithm_selection.options[hash_algorithm_selection.selectedIndex].value)).toString();
 
     } else if (credential_type_url.checked) {
-        hash_result.innerHTML = (await hasher(url_input.value, hash_algorithm_selection.options[hash_algorithm_selection.selectedIndex].value)).toString();
+        hash_result.innerHTML = (await hasher(url_input.value + salt_start_text + salt_input.value, hash_algorithm_selection.options[hash_algorithm_selection.selectedIndex].value)).toString();
     } else {
         return;
     }
